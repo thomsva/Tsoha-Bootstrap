c262e35999497abe7587fe4dc66478f1b1a7febf
Thomas Svartbäck <thomas.svartback@helsinki.fi>
2017-09-12 13:53:31 +0300

    Hello world kokeilu

--- app/controllers/hello_world_controller.php
+++ app/controllers/hello_world_controller.php
@@ -4,7 +4,8 @@
 
     public static function index(){
       // make-metodi renderöi app/views-kansiossa sijaitsevia tiedostoja
-   	  View::make('home.html');
+       //View::make('home.html');
+       echo 'Tämä on etusivu!';
     }
 
     public static function sandbox(){
