19e43aba148d57197b509dee56b5e1a060c57af9
Thomas Svartbäck <thomas.svartback@helsinki.fi>
2017-09-14 13:15:18 +0300

    Testejä

--- sql/add_test_data.sql
+++ sql/add_test_data.sql
@@ -1,1 +1,3 @@
 -- Lisää INSERT INTO lauseet tähän tiedostoon
+INSERT INTO Player (name, password) VALUES ('Kalle', 'Kalle123'); -- Koska id-sarakkeen tietotyyppi on SERIAL, se asetetaan automaattisesti
+INSERT INTO Player (name, password) VALUES ('Henri', 'Henri123');
--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -1,1 +1,6 @@
 -- Lisää CREATE TABLE lauseet tähän tiedostoon
+CREATE TABLE Player(
+  id SERIAL PRIMARY KEY, -- SERIAL tyyppinen pääavain pitää huolen, että tauluun lisätyllä rivillä on aina uniikki pääavain. Kätevää!
+  name varchar(50) NOT NULL, -- Muista erottaa sarakkeiden määrittelyt pilkulla!
+  password varchar(50) NOT NULL
+);
